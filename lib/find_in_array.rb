25e60cd7ae423b757da0e8d657846367b0ca0e83
Jacob G. Wazydrag <jgw.jakegeorge@gmail.com>
2020-06-22 01:27:24 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,9 +1,18 @@
 def find_element_index(array, value_to_find)
-  count = 0
+  count = 0 
   while count < array.length do
-    if array[count] == value_to_find
-      return count
-    end
-    count += 1
+  if array[count] == value_to_find
+    found_value_index = count 
   end
+  count += 1
+  end
+  found_value_index
 end
+
+p find_element_index([6, 5, 4, 3, 2, 1], 2)
+
+def find_element_index_easy (array, value_to_find)
+  array.index(value_to_find)
+end
+
+p find_element_index_easy(["Mom", "John", "Jacob", "Maggie"], "Mom")
